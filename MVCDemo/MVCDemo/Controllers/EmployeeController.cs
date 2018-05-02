16a5d21e36d547d97aa53708fe281f65e4579261
ManishaManekar <manishamanekar95@gmail.com>
2018-05-02 12:22:10 +0530

    Adding some files

---
+++ MVCDemo/MVCDemo/Controllers/EmployeeController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using MVCDemo.Models;
+
+namespace MVCDemo.Controllers
+{
+    public class EmployeeController : Controller
+    {
+        // GET: Employee
+        public ActionResult Index()
+        {
+            List<Employee> list = new List<Employee>();
+            list.Add(new Employee() {EmpId=1, EmpName="ABC", EmpSalary=10000 });
+            list.Add(new Employee() { EmpId = 2, EmpName = "XYZ", EmpSalary = 16000 });
+            list.Add(new Employee() { EmpId = 3, EmpName = "PQR", EmpSalary = 12000 });
+            list.Add(new Employee() { EmpId = 4, EmpName = "LMN", EmpSalary = 14000 });
+            ViewBag.Employees = list;
+            return View();
+        }
+    }
+}
---
+++ MVCDemo/MVCDemo/Models/Employee.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MVCDemo.Models
+{
+    public class Employee
+    {
+        public int EmpId { get; set; }
+        public string EmpName { get; set; }
+        public double  EmpSalary { get; set; }
+    }
+}
